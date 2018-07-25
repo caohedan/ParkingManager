aaf908a859d26e9499afb9887c42fc0df5a9b9ab
曹鹤丹 <amelia_cao@caohedandeMacBook-Pro.local>
2018-07-25 20:55:09 +0800

    add_model_receipt

--- src/main/java/com/oocl/ParkingManager/model/Receipt.java
+++ src/main/java/com/oocl/ParkingManager/model/Receipt.java
@@ -1,4 +1,13 @@
 package com.oocl.ParkingManager.model;
 
 public class Receipt {
+     String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
 }
